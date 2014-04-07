e9f73a7e39e7ad25db7b2481029be7a0c5daa352
Gueho Choi <guehocho@usc.edu>
2014-04-06 18:31:06 -0700

    Initial commit, loading agent

---
+++ src/agents/Master.java
@@ -0,0 +1,13 @@
+package agents;
+
+import agent.Agent;
+
+public class Master extends Agent {
+
+	@Override
+	protected boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
