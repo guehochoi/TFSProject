dfe231e1f81500f5f3743a1de7055bbbc8517f22
Marc Mendiola <mmendiol@usc.edu>
2014-04-10 22:22:09 -0700

    added test 5 skeleton

---
+++ src/Test/Test5.java
@@ -0,0 +1,34 @@
+package Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Master.FileSystem;
+
+/*	Test5:  Read the content of a TFS file and store it on the specified file on the local machine.
+ *  Input:  TFS file, local file path
+ *
+ *  Functionality:  If the TFS file does not exist then return an error message.  
+ *                  Otherwise, open the TFS file, read the content of the file, 
+ *                  write the content of the file to the local filesystem file.
+ *  
+ *  Example:  Test5 1\File1.png C:\MyDocument\Pic.png
+ *            If either 1\File1.png does not exist or 
+ *            C:\MyDocument\Pic.png exists then return the appropriate error message.  
+ *            Otherwise, open the TFS file 1\File1.png and read its content into memory.  
+ *            Create and open C:\MyDocument\Pic.png, write the retrieved content into it, and close this file.
+ *  */
+
+public class Test5 {
+	public static String rootDirectory = "\\usr";
+	public FileSystem fs;
+
+	public Test5() {
+		fs = new FileSystem();
+		fs.createDirectory(rootDirectory);
+	}
+	
+	
+	public static void main(String[] args) {
+    }
+}
